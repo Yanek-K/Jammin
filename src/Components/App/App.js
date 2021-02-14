78d0d2214ba21989030fe47d0f6416dfe0a76ab7
Yanek K <yanek.dev@gmail.com>
2021-02-13 19:19:56 -0500

    Wire Components and Styling

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,21 +1,51 @@
+import React from 'react'
 import "./App.css";
 import SearchBar from '../SearchBar/SearchBar'
+import SearchResults from '../SearchResults/SearchResults'
+import Playlist from '../Playlist/Playlist'
 
-function App() {
-  return (
-    <div className="App">
-      <div>
-        <h1>Ja<span className="highlight">mmm</span>ing</h1>
+class App extends React.Component {
+  constructor(props) {
+    super()
+    this.state = {
+      searchResults: [
+          {
+            name: "Brothers In Arms",
+            artist: "Mark Knopfler",
+            album: "Brothers In Arms",
+            id: 1
+          },
+          {
+            name: "Father and Son",
+            artist: "Cat Stevens",
+            album: "Tea for the Tillerman",
+            id: 2
+          },
+          {
+            name: "Sultans of Swing",
+            artist: "Dire Straits",
+            album: "Alchemy Live",
+            id: 3
+          }
+          
+      ]
+    }
+  }
+  render(){
+    return (
         <div className="App">
-          <SearchBar />
-          <div className="App-playlist">
-          {/* <!-- Add a SearchResults component -->
-          <!-- Add a Playlist component --> */}
+          <h1>Ja<span className="highlight">mmm</span>ing</h1>
+          <div>
+            <SearchBar />
+            <div className="App-playlist">
+              <SearchResults searchResults={this.state.searchResults}/>
+              <Playlist />
+            </div>
           </div>
         </div>
-      </div>
-    </div>
-  );
+    );
+
+  }
 }
 
 export default App;
